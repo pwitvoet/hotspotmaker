de54b4ed2d92a7d38fc1bf0c9f1260acffc46e1c
Pieter Witvoet <p.witvoet@gmail.com>
2026-08-07 01:55:27 +0200

    Unified the hotspot rectangle properties UI for single and multi-rectangle selections (it's all going through HotspotRectangleSelectionVM now). HotspotRectangleVM properties no longer create undoable actions, which allows other code to be simplified. Also fixed that rectangle coordinates and dimensions weren't updated in the hotspot rectangle list (right-side panel).

--- HotspotMaker/Hotspot/HotspotRectangleVM.cs
+++ HotspotMaker/Hotspot/HotspotRectangleVM.cs
@@ -1,5 +1,4 @@
-﻿using Avalonia;
-using HotspotMaker.History;
+﻿using HotspotMaker.History;
 using MLib.Mathematics.Spatial;
 using MLib.Texturing.Hotspotting;
 using System;
@@ -16,35 +15,55 @@ namespace HotspotMaker.Hotspot
         public double X
         {
             get => _x;
-            set => SetPropertyOngoing(v => _x = v, _x, value);
+            set
+            {
+                _x = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(DisplayName));
+            }
         }
 
         private double _y;
         public double Y
         {
             get => _y;
-            set => SetPropertyOngoing(v => _y = v, _y, value);
+            set
+            {
+                _y = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(DisplayName));
+            }
         }
 
         private double _width;
         public double Width
         {
             get => _width;
-            set => SetPropertyOngoing(v => _width = v, _width, value);
+            set
+            {
+                _width = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(DisplayName));
+            }
         }
 
         private double _height;
         public double Height
         {
             get => _height;
-            set => SetPropertyOngoing(v => _height = v, _height, value);
+            set
+            {
+                _height = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(DisplayName));
+            }
         }
 
         private bool _allowRotation;
         public bool AllowRotation
         {
             get => _allowRotation;
-            set => SetProperty(v => _allowRotation = v, _allowRotation, value);
+            set { _allowRotation = value; RaisePropertyChanged(); }
         }
 
         // TODO: Internally this is a None|Horizontal|Vertical enum, so it doesn't technically support horizontal + vertical (which is a 180 degree rotation)!
@@ -52,84 +71,84 @@ namespace HotspotMaker.Hotspot
         public bool AllowHorizontalMirroring
         {
             get => _allowHorizontalMirroring;
-            set => SetProperty(v => _allowHorizontalMirroring = v, _allowHorizontalMirroring, value);
+            set { _allowHorizontalMirroring = value; RaisePropertyChanged(); }
         }
 
         private bool _allowVerticalMirroring;
         public bool AllowVerticalMirroring
         {
             get => _allowVerticalMirroring;
-            set => SetProperty(v => _allowVerticalMirroring = v, _allowVerticalMirroring, value);
+            set { _allowVerticalMirroring = value; RaisePropertyChanged(); }
         }
 
         private HotspotLayout _horizontalLayout;
         public HotspotLayout HorizontalLayout
         {
             get => _horizontalLayout;
-            set => SetProperty(v => _horizontalLayout = v, _horizontalLayout, value);
+            set { _horizontalLayout = value; RaisePropertyChanged(); }
         }
 
         private HotspotLayout _verticalLayout;
         public HotspotLayout VerticalLayout
         {
             get => _verticalLayout;
-            set => SetProperty(v => _verticalLayout = v, _verticalLayout, value);
+            set { _verticalLayout = value; RaisePropertyChanged(); }
         }
 
         private double? _snapWidth;
         public double? SnapWidth
         {
             get => _snapWidth;
-            set => SetPropertyOngoing(v => _snapWidth = v, _snapWidth, value);
+            set { _snapWidth = value; RaisePropertyChanged(); }
         }
 
         private double? _snapHeight;
         public double? SnapHeight
         {
             get => _snapHeight;
-            set => SetPropertyOngoing(v => _snapHeight = v, _snapHeight, value);
+            set { _snapHeight = value; RaisePropertyChanged(); }
         }
 
         private double _selectionWeight;
         public double SelectionWeight
         {
             get => _selectionWeight;
-            set => SetPropertyOngoing(v => _selectionWeight = v, _selectionWeight, value);
+            set { _selectionWeight = value; RaisePropertyChanged(); }
         }
 
         private bool _isTopConcave;
         public bool IsTopConcave
         {
             get => _isTopConcave;
-            set => SetProperty(v => _isTopConcave = v, _isTopConcave, value);
+            set { _isTopConcave = value; RaisePropertyChanged(); }
         }
 
         private bool _isRightConcave;
         public bool IsRightConcave
         {
             get => _isRightConcave;
-            set => SetProperty(v => _isRightConcave = v, _isRightConcave, value);
+            set { _isRightConcave = value; RaisePropertyChanged(); }
         }
 
         private bool _isBottomConcave;
         public bool IsBottomConcave
         {
             get => _isBottomConcave;
-            set => SetProperty(v => _isBottomConcave = v, _isBottomConcave, value);
+            set { _isBottomConcave = value; RaisePropertyChanged(); }
         }
 
         private bool _isLeftConcave;
         public bool IsLeftConcave
         {
             get => _isLeftConcave;
-            set => SetProperty(v => _isLeftConcave = v, _isLeftConcave, value);
+            set { _isLeftConcave = value; RaisePropertyChanged(); }
         }
 
         private string[] _labels = Array.Empty<string>();
         public string[] Labels
         {
             get => _labels;
-            set => SetPropertyOngoing(v => _labels = v, _labels, value);
+            set { _labels = value; RaisePropertyChanged(); }
         }
 
 
@@ -144,30 +163,27 @@ namespace HotspotMaker.Hotspot
         public HotspotRectangleVM(HotspotRectangle rectangle, UndoSystem undoSystem)
             : base(undoSystem)
         {
-            WithoutChangeTracking(() =>
-            {
-                X = rectangle.Rectangle.X;
-                Y = rectangle.Rectangle.Y;
-                Width = rectangle.Rectangle.Width;
-                Height = rectangle.Rectangle.Height;
+            X = rectangle.Rectangle.X;
+            Y = rectangle.Rectangle.Y;
+            Width = rectangle.Rectangle.Width;
+            Height = rectangle.Rectangle.Height;
 
-                AllowRotation = rectangle.AllowRotation;
-                AllowHorizontalMirroring = rectangle.AllowedMirroring.HasFlag(Mirrorings.Horizontal);
-                AllowVerticalMirroring = rectangle.AllowedMirroring.HasFlag(Mirrorings.Vertical);
+            AllowRotation = rectangle.AllowRotation;
+            AllowHorizontalMirroring = rectangle.AllowedMirroring.HasFlag(Mirrorings.Horizontal);
+            AllowVerticalMirroring = rectangle.AllowedMirroring.HasFlag(Mirrorings.Vertical);
 
-                HorizontalLayout = rectangle.HorizontalLayout;
-                VerticalLayout = rectangle.VerticalLayout;
-                SnapWidth = rectangle.SnapWidth;
-                SnapHeight = rectangle.SnapHeight;
+            HorizontalLayout = rectangle.HorizontalLayout;
+            VerticalLayout = rectangle.VerticalLayout;
+            SnapWidth = rectangle.SnapWidth;
+            SnapHeight = rectangle.SnapHeight;
 
-                SelectionWeight = rectangle.SelectionWeight;
-                IsTopConcave = rectangle.ConcaveEdges.HasFlag(ConcaveEdges.Top);
-                IsRightConcave = rectangle.ConcaveEdges.HasFlag(ConcaveEdges.Right);
-                IsBottomConcave = rectangle.ConcaveEdges.HasFlag(ConcaveEdges.Bottom);
-                IsLeftConcave = rectangle.ConcaveEdges.HasFlag(ConcaveEdges.Left);
+            SelectionWeight = rectangle.SelectionWeight;
+            IsTopConcave = rectangle.ConcaveEdges.HasFlag(ConcaveEdges.Top);
+            IsRightConcave = rectangle.ConcaveEdges.HasFlag(ConcaveEdges.Right);
+            IsBottomConcave = rectangle.ConcaveEdges.HasFlag(ConcaveEdges.Bottom);
+            IsLeftConcave = rectangle.ConcaveEdges.HasFlag(ConcaveEdges.Left);
 
-                Labels= rectangle.Labels.ToArray();
-            });
+            Labels = rectangle.Labels.ToArray();
         }
 
         public HotspotRectangle CreateHotspotRectangle()
@@ -196,44 +212,16 @@ namespace HotspotMaker.Hotspot
         }
 
 
+        // TODO: This can be removed, because HotspotRectangleVM doesn't create any undoable actions anymore!
         public void WithoutUndo(Action action)
-            => WithoutChangeTracking(action);
-
-        // TODO: Selecting another rectangle should stop any ungoing actions such as this one! -- The editor should manage that, not the individual rectangle VMs!
-        public void MoveWithUndo(Vector offset)
-        {
-            var oldPosition = new Point(X, Y);
-            var newPosition = new Point(X + offset.X, Y + offset.Y);
-
-            PerformUndoableActionOngoing(
-                "Move",
-                () =>
-                {
-                    WithoutChangeTracking(() =>
-                    {
-                        X = newPosition.X;
-                        Y = newPosition.Y;
-                    });
-                },
-                () =>
-                {
-                    WithoutChangeTracking(() =>
-                    {
-                        X = oldPosition.X;
-                        Y = oldPosition.Y;
-                    });
-                });
-        }
+            => action();
 
         public void SetDimensionsWithoutUndo(double x, double y, double width, double height)
         {
-            WithoutChangeTracking(() =>
-            {
-                X = x;
-                Y = y;
-                Width = width;
-                Height = height;
-            });
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
         }
     }
 }
